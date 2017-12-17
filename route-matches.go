253f768da7bcc53c6dae124e98f4e47e7fac9345
Fisher <fisher.subs@gmail.com>
2017-12-17 17:15:06 +0800

    [route-matches] Removed fmt.Println().

--- route-matches.go
+++ route-matches.go
@@ -1,9 +1,5 @@
 package wstf
 
-import (
-	"fmt"
-)
-
 // If the given path matches this route,
 // it should exactly match the given pattern.
 func (m Route) Match(remainingPath string, request Request, response Response) bool {
@@ -17,11 +13,9 @@ func (m Route) Match(remainingPath string, request Request, response Response) b
 		for i := 0; i < len(m.ParamNames); i++ {
 			request.Params[m.ParamNames[i]] = matches[0][i+1]
 		}
-		fmt.Println(Stringify(request))
 	}
 	return true
 	matches2 := matches[0][1:]
-	fmt.Println("Matching: ", m.Pattern, remainingPath, matches, matches2)
 	if len(matches2) > 0 {
 		return true
 	} else {
@@ -30,7 +24,6 @@ func (m Route) Match(remainingPath string, request Request, response Response) b
 	i := 0
 	for _, match := range matches {
 		if len(match) != 0 {
-			fmt.Println("Matches!")
 			return true
 		} else {
 			i++
@@ -48,7 +41,6 @@ func (m Route) MatchPrefixPath(remainingPath string, request Request, response R
 		return false, ""
 	}
 	matches2 := matches[0][1:]
-	fmt.Println("Matches Children: ", m.Pattern, remainingPath, matches, matches2)
 	if len(m.ParamNames) > 0 {
 		for i := 0; i < len(m.ParamNames); i++ {
 			request.Params[m.ParamNames[i]] = matches[0][i+1]
@@ -58,7 +50,6 @@ func (m Route) MatchPrefixPath(remainingPath string, request Request, response R
 	i := 0
 	for _, match := range matches {
 		if len(match) != 0 {
-			fmt.Println("Matches!")
 			return true, matches2[len(matches2)-1]
 		} else {
 			i++
