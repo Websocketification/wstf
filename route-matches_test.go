431b26e4534fcbd863119f1b48b117c8cc71c602
Fisher <fisher.subs@gmail.com>
2017-12-17 17:20:48 +0800

    [route-matches_test.go] Added MatchAsteriskMark(), and MatchRegexp() tests.

--- route-matches_test.go
+++ route-matches_test.go
@@ -7,40 +7,50 @@ import (
 func TestRoute_Match(t *testing.T) {
 	TestRoute_MatchFull(t)
 	TestRoute_MatchParameterPattern(t)
+	TestRoute_MatchAsteriskMark(t)
+	TestRoute_MatchRegexp(t)
 }
 
 func TestRoute_MatchFull(t *testing.T) {
-	testMatch(t, `/users`, "/users", true)
-	testMatch(t, `/users`, "/user", false)
+	testMatch(t, `/users`, "/users", true, true, "")
+	testMatch(t, `/users`, "/user", false, false, "")
+	testMatch(t, `/users`, "/users/fisher", false, true, "/fisher")
 }
 
 func TestRoute_MatchParameterPattern(t *testing.T) {
-	testMatch(t, `/users/{userName}`, "/users/fisher", true)
-	testMatch(t, `/users-{userName}`, "/users-fisher", true)
-	//testMatch(t, `/users/{userName:[\w\W]+`, "/users/fisher", true)
-	testMatch(t, `/users/:userName`, "/users/fisher", false)
+	testMatch(t, `/users/{userName}`, "/users/fisher", true, true, "")
+	testMatch(t, `/users-{userName}`, "/users-fisher", true, true, "")
+	testMatch(t, `/users/:userName`, "/users/fisher", false, false, "")
 }
 
-func TestRoute_MatchChildren(t *testing.T) {
-	testMatchChildren(t, `/users`, "/users", true, "")
-	testMatchChildren(t, `/users`, "/users/fisher", true, "/fisher")
-	testMatchChildren(t, `/users`, "/users-fisher", true, "-fisher")
-	testMatchChildren(t, `/users/{userName}`, "/users/fisher/profile", true, "/profile")
+// Test the `*` asterisk mark.
+func TestRoute_MatchAsteriskMark(t *testing.T) {
+	testMatch(t, `/users/{userName}/*`, "/users/fisher", false, false, "")
+	testMatch(t, `/users/{userName}/*`, "/users/fisher/Whatever", true, true, "")
+	testMatch(t, `/users/{userName}/*`, "/users/fisher/Whatever/etc", false, true, "/etc")
 }
 
-// Test match RouteMethod.
-func testMatch(t *testing.T, pattern, path string, expected bool) {
+func TestRoute_MatchRegexp(t *testing.T) {
+	testMatch(t, `/users/{userName:[a-zA-Z0-9-_]+}`, "/users/zb-fisher", true, true, "")
+	testMatch(t, `/users/{userName:[a-zA-Z0-9-_]+}`, "/users/zb.fisher", false, true, ".fisher")
+	testMatch(t, `/users/{userName:[a-zA-Z0-9-_]+}`, "/users/zb/fisher", false, true, "/fisher")
+	testMatch(t, `/files/{filename:[a-zA-Z0-9-_]+}\.{extension:[a-zA-Z0-9]+}`, "/files/caillou.mp3", true, true, "")
+	//testMatch(t, `/files/{filename:[a-zA-Z0-9-_]+}\.{extension:[a-zA-Z0-9]{1,4}}`, "/files/caillou.mp3", true, true, "")
+}
+
+// Test Route#MatchPath() and Route#MatchChildren() methods.
+func testMatch(t *testing.T, pattern, path string, expected bool, expectedMatchChildren bool, expectedRemainingPath string) {
 	route := NewRoute(pattern, nil)
-	matches := route.Match(path, *NewFakeRequest(), *NewFakeResponse())
-	if matches != expected {
-		t.Fatal("Failed to match path, expected [", expected, "]: ", pattern, path)
+	req := *NewFakeRequest()
+	res := *NewFakeResponse()
+	matched := route.MatchPath(path, req, res)
+	if matched != expected {
+		t.Fatal("Failed to match path for [", pattern, path, "], expected [", expected, "] vs got [", matched, "]")
 	}
-}
 
-func testMatchChildren(t *testing.T, pattern, path string, expected bool, expectedRemainingPath string) {
-	route := NewRoute(pattern, nil)
-	matched, remainingPath := route.MatchPrefixPath(path, *NewFakeRequest(), *NewFakeResponse())
-	if matched != expected || expectedRemainingPath != remainingPath {
-		t.Fatal("Failed to match children, expected [", expected, expectedRemainingPath, "]: ", pattern, path, remainingPath)
+	// Test matching children.
+	matched, remainingPath := route.MatchChildren(path, req, res)
+	if matched != expectedMatchChildren || expectedRemainingPath != remainingPath {
+		t.Fatal("Failed to match children for [", pattern, path, "], expected [", expectedMatchChildren, expectedRemainingPath, "] vs got [", matched, remainingPath, "]")
 	}
 }
