dd29a020c6b3a14c0224fec2cc09ea5645e7e8f0
Fisher <fisher.subs@gmail.com>
2018-02-25 16:14:20 +0800

    [route-methods] Used singular word 'processor' as parameters.

--- route-methods.go
+++ route-methods.go
@@ -7,28 +7,28 @@ import (
 // General method that matches all method.
 const MethodAll = "ALL"
 
-func (m *Route) All(processors Processor) *Route {
-	return m.ListenMethod(MethodAll, processors)
+func (m *Route) All(processor Processor) *Route {
+	return m.ListenMethod(MethodAll, processor)
 }
 
-func (m *Route) Get(processors Processor) *Route {
-	return m.ListenMethod(http.MethodGet, processors)
+func (m *Route) Get(processor Processor) *Route {
+	return m.ListenMethod(http.MethodGet, processor)
 }
 
-func (m *Route) Post(processors Processor) *Route {
-	return m.ListenMethod(http.MethodPost, processors)
+func (m *Route) Post(processor Processor) *Route {
+	return m.ListenMethod(http.MethodPost, processor)
 }
 
-func (m *Route) Patch(processors Processor) *Route {
-	return m.ListenMethod(http.MethodPatch, processors)
+func (m *Route) Patch(processor Processor) *Route {
+	return m.ListenMethod(http.MethodPatch, processor)
 }
 
-func (m *Route) Put(processors Processor) *Route {
-	return m.ListenMethod(http.MethodPut, processors)
+func (m *Route) Put(processor Processor) *Route {
+	return m.ListenMethod(http.MethodPut, processor)
 }
 
-func (m *Route) Delete(processors Processor) *Route {
-	return m.ListenMethod(http.MethodDelete, processors)
+func (m *Route) Delete(processor Processor) *Route {
+	return m.ListenMethod(http.MethodDelete, processor)
 }
 
 // Add processors to listen specific method.
